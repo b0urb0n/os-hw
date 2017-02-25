cb244194160cc07b60b2baf62a6949ef247d9077
Matthew Miller <usr.bin.bourbon@gmail.com>
2017-02-25 16:06:52 -0600

    formatting fixes

--- src/miller/paging/Paging.java
+++ src/miller/paging/Paging.java
@@ -5,15 +5,14 @@ public class Paging {
       + "\t* N frames will control how much paging will happen in order to solve the problem.\n";
 
   public static void main(String[] args) {
-    if (args.length == 0){ //TODO change this back to 1 before prod
-      //Integer physicalFrames = Integer.valueOf(args[0]);
-      Integer physicalFrames = 4;
+    if (args.length == 1) {
+      Integer physicalFrames = Integer.valueOf(args[0]);
       
       Menu m = new Menu(physicalFrames);
-      while (true){
+      while (true) {
         m.display();
       }
-    }else{
+    } else {
       System.out.println(HELP);
     }
   }
