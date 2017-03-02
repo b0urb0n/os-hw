b433cedfc59944792257943c5c3d3f31f83ec1af
Matt Miller <usr.bin.bourbon@gmail.com>
2017-03-01 22:01:57 -0600

    added try/except in case user gives something other than an integer

--- src/miller/paging/Paging.java
+++ src/miller/paging/Paging.java
@@ -1,12 +1,18 @@
 package miller.paging;
 
 public class Paging {
+  private static Integer physicalFrames;
   private static final String HELP = "Usage:\tjava FinalProject <N frames>\n\n"
       + "\t* N frames will control how much paging will happen in order to solve the problem.\n";
 
   public static void main(String[] args) {
     if (args.length == 1) {
-      Integer physicalFrames = Integer.valueOf(args[0]);
+      try {
+        physicalFrames = Integer.valueOf(args[0]);
+      } catch (Exception e) {
+        System.out.println(HELP);
+        System.exit(1);
+      }
       
       Menu m = new Menu(physicalFrames);
       while (true) {
